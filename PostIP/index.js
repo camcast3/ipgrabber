750618dd968f9a0d5c667e35369a2e77c0fe2e8b
camcast3 <carltoncameron50@gmail.com>
2023-05-03 18:47:12 -0700

    new azure function to grab ip from friends for allow listing

---
+++ PostIP/index.js
@@ -0,0 +1,68 @@
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING)
+
+module.exports = async function (context, req) {
+    try {
+        const database = await mongoClient.db(process.env.MONGODB_DATABSE);
+        const collection = database.collection(process.env.MONGODB_COLLECTION);
+        
+        const { MongClient } = require("mongodb");
+
+        const callerip = req.get('X-Forwarded-For');
+        
+        if (!callerip) {
+            context.res = {
+                "headers": {
+                    "content-type": "application/json"
+                },
+                "body": {
+                    "message": "This HTTP triggered function executed successfully. Unable to detect your IP address. Please contact CAMM."
+                }
+            };
+            return
+        } 
+        
+        const iprange = callerip + '/32';
+        const name = (req.query.name || (req.body && req.body.name));
+
+        if (!name){
+            context.res = {
+                "headers": {
+                    "content-type": "application/json"
+                },
+                "body": {
+                    "message": "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for your IP to be saved."
+                }
+            };
+            return
+        } 
+
+        await collection.insertOne(
+            { 
+                "name": name, 
+                "iprange": iprange
+            }
+        );
+
+        context.res = {
+            "headers": {
+                "content-type": "application/json"
+            },
+            "body": {
+                "message": "Successfully added entry: name = " + name + ", ipranges = " + iprange 
+            }
+        };
+    
+    } catch (error) {
+        context.res = {
+            "status": 500,
+            "headers": {
+                "content-type": "application/json"
+            },
+            "body": {
+                "message": error.toString()
+            }
+        };
+    }
+}
